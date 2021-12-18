acf4948df3c05bc510d371be6b102773aa96c4f8
Konteh <kontehebrima993@gmail.com>
2021-12-17 22:25:06 -0800

    initial commit

---
+++ project.js
@@ -0,0 +1,65 @@
+let projects = [
+    {
+        name: "All",
+        tags: "#streetShooting, #indoorShooting, #creativeShoot",
+        image: "project (1).JPEG",
+    },
+    
+    {
+        name: "Street Shooting",
+        tags: "#streetShooting",
+        image: "project (3).JPEG",
+    },
+    {
+        name: "Street Shooting",
+        tags: "#streetShooting",
+        image: "project (8).JPEG",
+    },
+    {
+        name: "Street Shooting",
+        tags: "#streetShooting",
+        image: "project (11).JPEG",
+    },
+    {
+        name: "Street Shooting",
+        tags: "#streetShooting",
+        image: "project (12).JPEG",
+    },
+    {
+        name: "Indoor Shooting",
+        tags: "#indoorShooting",
+        image: "project (6).JPEG",
+    },
+    {
+        name: "Indoor Shooting",
+        tags: "#indoorShooting",
+        image: "project (9).JPEG",
+    },
+    {
+        name: "Indoor Shooting",
+        tags: "#indoorShooting",
+        image: "project (7).JPEG",
+    },
+    {
+        name: "creative shooting",
+        tags: "#creativeShoot",
+        image: "project (4).JPEG",
+    },
+    {
+        name: "creative shooting",
+        tags: "#creativeShoot",
+        image: "project (14).JPEG",
+    },
+    {
+        name: "creative shooting",
+        tags: "#creativeShoot",
+        image: "project (13).JPEG",
+    },
+    {
+        name: "creative shooting",
+        tags: "#creativeShoot",
+        image: "project (2).JPEG",
+    },
+  
+    
+]
